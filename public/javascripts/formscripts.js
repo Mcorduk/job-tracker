ea13da74f7341ee72469d126a5b382df7669c785
Mcorduk <om.corduk@gmail.com>
2024-01-19 20:12:47 +0300

    feat: create form ejs as well as scripts for validation and DOM manipulation, pass dynamic dates via router

---
+++ public/javascripts/formscripts.js
@@ -0,0 +1,29 @@
+function toggleRepeatingFrequency() {
+  const repeatingFrequencyContainer = document.getElementById(
+    "repeatingFrequencyContainer",
+  );
+  if (document.getElementById("repeating").checked) {
+    repeatingFrequencyContainer.style.display = "block";
+  } else {
+    repeatingFrequencyContainer.style.display = "none";
+  }
+}
+
+function validateDateAndTime() {
+  const dateInput = document.getElementById("date");
+  const errorSpan = document.getElementById("date-error");
+
+  // Check if date is in the future or, if today, if time is later than current time
+  if (
+    selectedDate < now ||
+    (selectedDate.toDateString() === now.toDateString() &&
+      selectedDate.getTime() <= now.getTime())
+  ) {
+    errorSpan.textContent =
+      "Date and time must be in the future or a later time today.";
+    return false; // Prevent form submission
+  }
+  // Validation passed, clear any previous error message
+  errorSpan.textContent = "";
+  return true;
+}
