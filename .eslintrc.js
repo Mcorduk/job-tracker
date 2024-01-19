c6568915ba47f92029935d8c67d0da59b284c53a
Mcorduk <om.corduk@gmail.com>
2024-01-19 14:26:07 +0300

    chore: add and adjust prettier and eslint.
    write configs, rules and populate ignore files for git and linters

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    node: true, // Set node environment for server-side code
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ["**/*.js"], // Apply these settings to all JavaScript files
+      excludedFiles: "public/*", // Exclude public folder from linting
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    // Add or override rules as needed for your Express.js project
+    "no-console": "off", // Allowing console.log in server-side code
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+    "no-unused-vars": ["error", { argsIgnorePattern: "^next$|^req$" }],
+  },
+  plugins: ["import"],
+  settings: {
+    "import/resolver": {
+      node: {},
+    },
+  },
+};
